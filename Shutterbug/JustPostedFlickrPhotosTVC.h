d949e2e08c8113b88a9601925602f37bf7ee24af
emil <emil@emils-Mac.local>
2014-12-07 15:49:42 -0800

    Original commit for shutterBug base for HW 5

---
+++ Shutterbug/JustPostedFlickrPhotosTVC.h
@@ -0,0 +1,13 @@
+//
+//  JustPostedFlickrPhotosTVC.h
+//  Shutterbug
+//
+//  Created by Sameh Fakhouri on 11/24/14.
+//  Copyright (c) 2014 Lehman College. All rights reserved.
+//
+
+#import "FlickrPhotosTVC.h"
+
+@interface JustPostedFlickrPhotosTVC : FlickrPhotosTVC
+
+@end
